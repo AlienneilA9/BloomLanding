eb3d8d89ef0da869fe4196a1e6df57422ac94d1c
AlienneilA9 <97284854+AlienneilA9@users.noreply.github.com>
2022-05-30 13:32:42 +0200

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,8 @@
+
+    import {Page1} from "./Page1";
+
+    export default function App() {
+        document.body.style.backgroundColor = '#f5f5f5';
+        document.body.style.margin = '0';
+        return <Page1 />;
+    }
---
+++ src/Page1.js
@@ -0,0 +1,10 @@
+import React from "react";
+import styled from "styled-components";
+export const Page1 = ({}) => {
+  return <PageRoot src={"https://file.rendit.io/n/1f8HBQpaaWhiIGEdrhLp.png"} />;
+};
+const PageRoot = styled.img`
+  height: 2121.02px;
+  margin: auto;
+  min-width: 3829.98px;
+`;
